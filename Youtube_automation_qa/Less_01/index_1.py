aa4b94698a4b76601ff2168585e094fe208195c8
PrintCompany <djkif1212@gmail.com>
2023-11-06 17:21:21 +0200

    at work

--- Youtube_automation_qa/Less_01/index_1.py
+++ Youtube_automation_qa/Less_01/index_1.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome()
 
 browser.get("https://www.qa-practice.com/elements/button/simple")
-
+browser.find_element(By.ID, "submit-id-submit")
 
 
 
