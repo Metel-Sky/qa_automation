c1d5dbb63caf413ae498e5b2490532c47eb9dfdf
PrintCompany <djkif1212@gmail.com>
2023-11-07 10:45:22 +0200

    at work

--- Youtube_automation_qa/Less_01/index_1.py
+++ Youtube_automation_qa/Less_01/index_1.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 browser = webdriver.Chrome()
 
 browser.get("https://www.qa-practice.com/elements/button/simple")
-browser.find_element(By.ID, "submit-id-submit")
+click_button = browser.find_element(By.ID, "submit-id-submit")
+sleep(2)
+click_button.click()
 
 
-
-# sleep(5)
+sleep(3)
