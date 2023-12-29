a333b11b8f977649229c15eb7bab2257331b59bf
Vinch <djkif1212@gmail.com>
2023-12-29 17:13:29 +0200

    Home Work 10

--- Lesson_10/Home_Work_10.py
+++ Lesson_10/Home_Work_10.py
@@ -3,15 +3,12 @@ import pytest
 
 def add_three_numbers(number_1: int | float, number_2: int | float, number_3: int | float) -> int | float:
     result = number_1 + number_2 + number_3
+    with open("log.txt", "a") as file:
+        file.write(f"\n{number_1}, {number_2}, {number_3} => {result}\n")
     return result
 
+# with open("log.txt", "a") as file:
+#     file.write(f"\n{number_1}, {number_2}, {number_3} => {result}\n")
 
-@pytest.mark.parametrize("number_1, number_2, number_3, expected_result", [
-    pytest.param(3, 5, 3, 11, id="standard"),
-    pytest.param(2, 5, 3, 10, id="negative"),
-    pytest.param(4, 5, 3, 12, id="magic")
-])
-def test_add_three_numbers(number_1, number_2, number_3, expected_result):
-    assert add_three_numbers(number_1, number_2, number_3) == expected_result
-    with open("log.txt", "a") as file:
-        file.write(f"\n{number_1}, {number_2}, {number_3} => {expected_result}\n")
+
+
---
+++ Lesson_10/test_hw.py
@@ -0,0 +1,9 @@
+import pytest
+from Home_Work_10 import add_three_numbers
+@pytest.mark.parametrize("number_1, number_2, number_3, expected_result", [
+    pytest.param(3, 5, 36, 44, id="standard"),
+    pytest.param(2, 5, 3, 10, id="negative"),
+    pytest.param(4, 5, 3, 12, id="magic")
+])
+def test_add_three_numbers(number_1, number_2, number_3, expected_result):
+    assert add_three_numbers(number_1, number_2, number_3) == expected_result
