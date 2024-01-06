b4257049cf45b774eef1c25908db73256a820b25
Vinch <djkif1212@gmail.com>
2024-01-06 13:59:53 +0200

    Home Work 12

--- Lesson_12/Home_work_12/test_chuknorris.py
+++ Lesson_12/Home_work_12/test_chuknorris.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 
 
 @pytest.mark.usefixtures("fixture_random")
@@ -7,22 +8,31 @@ class TestRandom:
         assert int(self.response.json()["created_at"][:4]) > 1995, "all our jokes were created until 1990"
 
     def test_status_code(self):
-        assert self.status_code ==200 #Перевірка статус коду в evaluate
+        assert self.status_code == 200  # Перевірка статус коду в evaluate
 
 
 def test_category():
-    pass
+    URL = "https://api.chucknorris.io/jokes/categories"
+    response = requests.request(method="GET", url=URL)
+    print(response.json())
 
 
+# def test_categories():
+#     URL = "https://api.chucknorris.io/jokes/random?category=career"
+#     response = requests.request(method="GET", url=URL)
+    # print(response.text)
+    # print()
+    # print(response.json()["id"])
+    # print()
+    # assert len(response.json()["id"]) > 10
 
+@pytest.mark.parametrize("category",
+                         requests.request(method="GET", url="https://api.chucknorris.io/jokes/categories").json())
 
-
-
-
-
-
-
-
+def test_categories(category):
+    URL = f"https://api.chucknorris.io/jokes/random?category={category}"
+    response = requests.request(method="GET", url=URL)
+    assert len(response.json()["id"]) >10
 
 
 
@@ -45,11 +55,3 @@ def test_category():
 #   response.json()   інформація в форматі json
 #   response.json()["created_at"] == дата створення
 #
-
-
-
-
-
-
-
-
