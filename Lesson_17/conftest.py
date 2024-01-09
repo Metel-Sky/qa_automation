8485c30bccdb4c29860efa47063f3a81f0fe5bdd
Vinch <djkif1212@gmail.com>
2024-01-09 21:48:42 +0200

    Home Work 18

--- Lesson_17/conftest.py
+++ Lesson_17/conftest.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 @pytest.fixture
 def chrome():
-    driver = webdriver.Chrome(executable_path="/Users/pavlokostyshen/Desktop/hillel/Hillel_october_23/lesson_17/chromedriver")
+    driver = webdriver.Chrome(executable_path="\Users\Vinch\Desktop\QA_automation\Lesson_18/chromedriver")
     yield driver
     driver.quit()
---
+++ Lesson_18/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def chrome():
+    driver = webdriver.Chrome(executable_path="Users\Vinch\Desktop\QA_automation\Lesson_18\chromedriver.exe")
+    yield driver
+    driver.quit()
