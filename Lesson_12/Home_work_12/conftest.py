45a8ba7cd85c861c969886628b6566c91568b0b6
Vinch <djkif1212@gmail.com>
2024-01-10 16:48:46 +0200

    Home Work 12

--- Lesson_12/Home_work_12/conftest.py
+++ Lesson_12/Home_work_12/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import requests
 
+from Lesson_12.Home_work_12.test_chuknorris import ChuckNorrisJokeSearch
+
 
 @pytest.fixture(scope="class")
 def fixture_random(request):
@@ -9,3 +11,8 @@ def fixture_random(request):
     request.cls.response = response
     request.cls.status_code = status_code
     yield response
+
+
+@pytest.fixture
+def joke_search():
+    return ChuckNorrisJokeSearch()
--- Lesson_12/Home_work_12/test_chuknorris.py
+++ Lesson_12/Home_work_12/test_chuknorris.py
@@ -4,8 +4,8 @@ import requests
 
 @pytest.mark.usefixtures("fixture_random")
 class TestRandom:
-    # перевірити поля "icon_url" чи він не пустий + чи це картинка - 2 теста
-    # перевірити чи є ключ "value"  і перевірити його значення - 2 теста
+    # перевірити поля "icon_url" чи він не пустий + чи це картинка - 2 теста  ^-^
+
     def test_chuk_year(self):
         assert int(self.response.json()["created_at"][:4]) > 1995, "all our jokes were created until 1990"
 
@@ -32,33 +32,91 @@ class TestRandom:
         icon = response.json()
         assert icon['icon_url'].endswith('.jpg') != True  # Заглушка
 
+    # ========================================================================================
+    # перевірити чи є ключ "value"  і перевірити його значення - 2 теста
+
+    def test_value_key_exists(self):
+        chuck_api_url = 'https://api.chucknorris.io/jokes/random'
+        response = requests.get(chuck_api_url)
+        value = response.json()
+
+        assert 'value' in value
+
+    def test_value_key_content(self):
+        chuck_api_url = 'https://api.chucknorris.io/jokes/random'
+        response = requests.get(chuck_api_url)
+        value = response.json()
+
+        assert 'value' in value
+        assert value['value'] is not None
+        assert len(value['value']) > 0
+
+
+# Зробити окремий клас
+class ChuckNorrisJokeSearch:
+    def __init__(self):
+        self.base_url = 'https://api.chucknorris.io/jokes/search'
+
+    def search_jokes_by_keyword(self, query):
+        url = f'{self.base_url}?query={query}'
+        response = requests.get(url)
+        return response.status_code, response.json()
+
+
+def test_search_jokes_status_code(joke_search):
+    status_code, _ = joke_search.search_jokes_by_keyword('python')
+    assert status_code == 200
+
+
+def test_search_jokes_count(joke_search):
+    _, data = joke_search.search_jokes_by_keyword('testing')
+    assert 'total' in data
+    assert data['total'] >= 0
+
+
+def test_search_jokes_content(joke_search):
+    _, data = joke_search.search_jokes_by_keyword('Chuck')
+    assert 'result' in data
+    assert isinstance(data['result'], list)
+    assert len(data['result']) > 0
+    assert 'value' in data['result'][0]
+
+
+def test_search_jokes_invalid_keyword(joke_search):
+    status_code, data = joke_search.search_jokes_by_keyword('invalid_keyword')
+    assert status_code == 200
+    assert 'total' in data
+    assert data['total'] == 0
+
+
+def test_search_jokes_special_characters(joke_search):
+    status_code, data = joke_search.search_jokes_by_keyword('$%^&*')
+    assert status_code == 200
+    assert 'total' in data
+    assert data['total'] == 0
+
+
 def test_category():
     URL = "https://api.chucknorris.io/jokes/categories"
     response = requests.request(method="GET", url=URL)
     print(response.json())
 
 
-# def test_categories():
-#     URL = "https://api.chucknorris.io/jokes/random?category=career"
-#     response = requests.request(method="GET", url=URL)
-# print(response.text)
-# print()
-# print(response.json()["id"])
-# print()
-# assert len(response.json()["id"]) > 10
-
-# @pytest.mark.parametrize("category",
-#                          requests.request(method="GET", url="https://api.chucknorris.io/jokes/categories").json())
-#
-# def test_categories(category):
-#     URL = f"https://api.chucknorris.io/jokes/random?category={category}"
-#     response = requests.request(method="GET", url=URL)
-#     assert len(response.json()["id"]) >10
+def test_categories():
+    URL = "https://api.chucknorris.io/jokes/random?category=career"
+    response = requests.request(method="GET", url=URL)
+    print(response.text)
+    print()
+    print(response.json()["id"])
+    print()
+    assert len(response.json()["id"]) > 10
 
 
-# todo evaluate
-#   assert response.status_code ==200 Перевірка статус коду в evaluate
-#   assert int(response.json()["created_at"][:4]) > 1995, "all our jokes were created until 1990"
-#   response.json()   інформація в форматі json
-#   response.json()["created_at"] == дата створення
-#======================================================
+@pytest.mark.parametrize("category",
+                         requests.request(method="GET", url="https://api.chucknorris.io/jokes/categories").json())
+def test_categories(category):
+    URL = f"https://api.chucknorris.io/jokes/random?category={category}"
+    response = requests.request(method="GET", url=URL)
+    assert len(response.json()["id"]) > 10
+
+# ======================================================
