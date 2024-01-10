72fd68858d64bfd1cf4ccf4b38975a1cfae65bea
Vinch <djkif1212@gmail.com>
2024-01-10 16:12:47 +0200

    Home Work 18

--- Lesson_18/tests/test_text_box.py
+++ Lesson_18/tests/test_text_box.py
@@ -1,5 +1,5 @@
 from Lesson_18.TextBoxPage import TextBoxPage
-import time
+
 
 
 class TestTextBox:
@@ -12,7 +12,7 @@ class TestTextBox:
         page.click_submit()
         name_field = page.get_result_fullname()
         assert name_field.replace("Name:", "") == "Pavlo"
-        time.sleep(5)
+
 
     def test_email_fill_and_check(self, chrome):
         page = TextBoxPage(chrome)
@@ -22,7 +22,7 @@ class TestTextBox:
         page.click_submit()
         email_field = page.get_result_email()
         assert email_field.replace("Email:", "") == "djkif1212@gmail.com"
-        time.sleep(5)
+
 
     def test_curr_addr_fill_and_check(self, chrome):
         page = TextBoxPage(chrome)
@@ -32,7 +32,6 @@ class TestTextBox:
         page.click_submit()
         curr_field = page.get_result_current_address()
         assert curr_field.replace("Current Address :", "") == "Якийсь адрес))"
-        time.sleep(5)
 
     def test_perm_addr_fill_and_check(self, chrome):
         page = TextBoxPage(chrome)
@@ -42,4 +41,3 @@ class TestTextBox:
         page.click_submit()
         perm_field = page.get_result_permanent_address()
         assert perm_field.replace("Permananet Address :", "") == "Якийсь текст про море))"
-        time.sleep(5)
