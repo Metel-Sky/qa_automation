713a9d8a0b34c87179bb68075fc9fc81d6590947
vinch <djkif1212@gmail.com>
2024-01-18 22:13:51 +0200

    Home Work 15

---
+++ Lesson_15/Home_Work/Home_Work_15.py
@@ -0,0 +1,24 @@
+
+
+list = [num for num in range(34,122)if num %3 ==0 and num %2==0]
+print(list)
+
+
+class Calc:
+    def __init__(self):
+        pass
+    def add(self,a,b):
+        return a+b
+    def subtract(self,a,b):
+        return a-b
+    def multiply(self,a,b):
+        return a*b
+    def divide(self,a,b):
+        if b!=0:
+            return a/b
+        else:
+            raise ValueError("На нуль ділити не можна!")
+
+    @staticmethod
+    def greet():
+        print("Привіт, я калькулятор.")
