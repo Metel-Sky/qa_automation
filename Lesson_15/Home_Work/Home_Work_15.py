dff3e45afa8dcd40461d3e99a368460e5d4e54be
vinch <djkif1212@gmail.com>
2024-01-18 22:25:11 +0200

    Home Work 15

--- Lesson_15/Home_Work/Home_Work_15.py
+++ Lesson_15/Home_Work/Home_Work_15.py
@@ -1,24 +1,34 @@
-
-
-list = [num for num in range(34,122)if num %3 ==0 and num %2==0]
+list = [num for num in range(34, 122) if num % 3 == 0 and num % 2 == 0]
 print(list)
 
 
 class Calc:
     def __init__(self):
         pass
-    def add(self,a,b):
-        return a+b
-    def subtract(self,a,b):
-        return a-b
-    def multiply(self,a,b):
-        return a*b
-    def divide(self,a,b):
-        if b!=0:
-            return a/b
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b != 0:
+            return a / b
         else:
             raise ValueError("На нуль ділити не можна!")
 
     @staticmethod
     def greet():
         print("Привіт, я калькулятор.")
+
+
+import random
+
+matrix = [[random.randint(0, 9) for _ in range(7)] for _ in range(7)]
+
+for row in matrix:
+    print(row)
--- Lesson_15/Home_Work/test_calc.py
+++ Lesson_15/Home_Work/test_calc.py
@@ -2,6 +2,7 @@ import datetime
 import pytest
 from Home_Work_15 import Calc
 
+
 @pytest.fixture(scope="class")
 def setup_teardown(request):
     with open("test_log.txt", "a") as file:
@@ -13,11 +14,13 @@ def setup_teardown(request):
 
     request.addfinalizer(teardown)
 
+
 @pytest.mark.parametrize("a, b, expected_result", [(2, 3, 5), (0, 0, 0), (-1, 1, 0), (10, -5, 5), (7, 8, 15)])
 def test_addition(a, b, expected_result, setup_teardown):
     calc = Calc()
     result = calc.add(a, b)
     assert result == expected_result
+
 
 @pytest.mark.parametrize("a, b, expected_result", [(6, 2, 3), (10, 2, 5), (15, 3, 5), (-8, 2, -4), (0, 1, 0)])
 def test_division(a, b, expected_result, setup_teardown):
