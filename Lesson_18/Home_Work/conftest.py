1f4921346a68f69543cee589d870a1ccc4c58f78
vinch <djkif1212@gmail.com>
2024-01-18 17:18:18 +0200

    fix in ubuntu

--- Lesson_18/Home_Work/conftest.py
+++ Lesson_18/Home_Work/conftest.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def chrome():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("executable_path=Users/Vinch/Desktop/QA_automation/Lesson_18/chromedriver.exe")
+    chrome_options.add_argument("executable_path=/home/vinch/Стільниця/Hilel/Lesson_18/Home_Work/chromedriver")
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
