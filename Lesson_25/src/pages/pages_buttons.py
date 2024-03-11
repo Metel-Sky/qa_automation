1236b2559276fa7766b80648121d440f6e4e20ac
Vinch <djkif1212@gmail.com>
2024-03-11 17:54:14 +0200

    26

--- Lesson_25/src/pages/pages_buttons.py
+++ Lesson_25/src/pages/pages_buttons.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from Lesson_24.widgets.components import Button
+from Lesson_25.widgets.components import Button
 
 class PageButtons:
     _instance = None
