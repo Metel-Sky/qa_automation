0eeea50ccede9928aafc74b8a4e1313c5c9e98d4
Vinch <djkif1212@gmail.com>
2024-03-21 21:26:14 +0200

    28

--- Lesson_28/Home_Work/conftest.py
+++ Lesson_28/Home_Work/conftest.py
@@ -1,11 +1,11 @@
 import pytest
+
 from selenium import webdriver
 
 
-@pytest.fixture
-def chrome():
+@pytest.fixture(scope="class")
+def chrome_class(request):
     driver = webdriver.Chrome(executable_path="C:\chromedriver")
+    request.cls.driver = driver
     yield driver
     driver.quit()
-
-
